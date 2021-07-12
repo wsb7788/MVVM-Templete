6bc7715e9ea4a829203867b699e85b8f30abbfcb
wsb7788 <wsb0514@naver.com>
2021-07-12 23:53:21 +0900

    loginactivity

---
+++ app/src/main/java/com/minux/template2021/ui/login/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.minux.template2021.ui.login
+
+import android.os.Bundle
+import com.minux.template2021.R
+import com.minux.template2021.ui.BaseActivity
+
+class LoginActivity: BaseActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
